95f5c4069444a30e139742416f2ee3d8c2ff3956
Ben Watts <ben@kilowatts.io>
2024-08-22 08:07:24 +0100

    add sentry mobile

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,7 +1,13 @@
-export const initSentry = () => {};
+import * as Sentry from '@sentry/react-native';
+
+export const initSentry = () => {
+  Sentry.init({
+    dsn: 'https://04af1e62e8dd9e1f153c9b63effe6466@o4505890996748288.ingest.us.sentry.io/4507819722211328',
+  });
+};
 
 export default initSentry;
 
 export const captureException = (e: Error) => {
-  console.error(e);
+  Sentry.captureException(e);
 };
